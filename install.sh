9e1f54905f500f7f28b9c527cad46d6974eb03ee
Hamees Sayed <98336593+hamees-sayed@users.noreply.github.com>
2022-11-03 20:13:56 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,3 @@ curl -sLS https://get.arkade.dev | sudo sh
 echo "export PATH=$PATH:$HOME/.arkade/bin" >> ~/.bashrc
 source ~/.bashrc
 export PATH=$PATH:$HOME/.arkade/bin/
-arkade get kubectl helm k3d
-k3d cluster create
