546176c02946323a0211083a3d1d550d79fb7f42
Hamees Sayed <98336593+hamees-sayed@users.noreply.github.com>
2022-11-05 00:49:47 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ echo "export PATH=$PATH:$HOME/.arkade/bin" >> ~/.bashrc
 source ~/.bashrc
 export PATH=$PATH:$HOME/.arkade/bin
 arkade get k3d kubectl helm 
-k3d cluster create 
+k3d cluster create demo-cluster
