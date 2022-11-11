407dc21285b8e5303ca6588ff5b807c696c3603e
Hamees Sayed <98336593+hamees-sayed@users.noreply.github.com>
2022-11-11 19:44:10 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@ curl -sLS https://get.arkade.dev | sudo sh
 echo "export PATH=$PATH:$HOME/.arkade/bin" >> ~/.bashrc
 source ~/.bashrc
 export PATH=$PATH:$HOME/.arkade/bin
+go install github.com/google/wire/cmd/wire@latest
 arkade get k3d kubectl helm 
 k3d cluster create demo-cluster
